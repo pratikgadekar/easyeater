6faacbc801c485449352a7ef9c7ec1dff5d5ba14
Anupam Sarfare <anupamsarfare@Anupams-Mac-mini.local>
2015-03-13 13:21:52 +0530

    adding lookup cloud code

---
+++ eayeater-cloud-code/cloud/lookup/lookup.js
@@ -0,0 +1,64 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+
+var Response = {
+    EmptyParameters: 'Some parameters empty',
+    InternalServerError: 'Oops! Some error occurred!',
+    SaveSuccess: 'User save successfully!'
+}
+
+
+exports.save = function(params) {
+ 
+    if(!params || !params.name || !params.type){
+        params.error(Response.EmptyParameters);
+    }else{
+    	   var currentUser = Parse.User.current();
+       
+        	var lookup = new Lookup();
+        	lookup.set("name",params.name);
+        	lookup.set("type",params.type);
+        	lookup.set("updatedAt",params.updatedAt);
+        	lookup.set("updatedBy",params.updatedBy); //currentUser
+        	lookup.set("createdAt",params.createdAt); 
+        	lookup.set("createdBy",params.createdBy); //currentUser
+            
+            lookup.save(null, {
+            success: function(lookup) {
+                params.success(Response.SaveSuccess);
+            },
+             error: function(error) {
+                  params.error(error);
+             }
+            });
+
+
+
+    } 
+}
+ 
+exports.update = function(params) {
+    if(!params || !params.id || !params.name || !params.type){
+        params.error(Response.EmptyParameters);
+    }
+    else{
+    	   var currentUser = Parse.User.current();
+       
+        	var lookup = new Lookup();
+        	lookup.set("name",params.name);
+        	lookup.set("type",params.type);
+        	lookup.set("updatedAt",params.updatedAt);
+        	lookup.set("updatedBy",params.updatedBy); //currentUser
+        	lookup.set("createdAt",params.createdAt); 
+        	lookup.set("createdBy",params.createdBy); //currentUser
+            lookup.save(null, {
+	            success: function(lookup) {
+	                params.success(Response.SaveSuccess);
+	            },
+	             error: function(error) {
+	                  params.error(error);
+                }
+            });
+        } 
+}
--- eayeater-cloud-code/cloud/main.js
+++ eayeater-cloud-code/cloud/main.js
@@ -1,6 +1,48 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+// Parse.Cloud.define("hello", function(request, response) {
+//   response.success("Hello world! 12356");
+// });
+
+var lookup = require('cloud/lookup/lookup.js');
+
+
+Parse.Cloud.define("lookupSave", function(req, res) {
+    if(req.params.id) {
+            lookup.update({
+                id:req.params.id,
+                name: req.params.name,
+                type: req.params.type,
+                updatedAt: req.params.updatedAt,
+                updatedBy: req.params.updatedBy,
+                createdAt: req.params.createdAt,
+                createdBy: req.params.createdBy,
+                isDisabled: req.params.isDisabled,
+                success: function(message) {
+                    res.success(message);
+                },
+                error: function(message) {
+                    res.error(message);
+                }
+            });
+    } else {
+ 
+           lookup.save({
+               name: req.params.name,
+               type: req.params.type,
+               updatedAt: req.params.updatedAt,
+               updatedBy: req.params.updatedBy,
+               createdAt: req.params.createdAt,
+               createdBy: req.params.createdBy,
+               isDisabled: req.params.isDisabled,
+               success: function(message) {
+                res.success(message);
+               },
+               error: function(message) {
+                res.error(message);
+               }
+            });
+}
 });
+
